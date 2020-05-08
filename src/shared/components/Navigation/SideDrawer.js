33abb670e4a1724f9915f15dc197bc607cb54ea7
Dika Aditia <aditiaaadicka@gmail.com>
2020-05-08 20:48:00 +0700

    add nav styling for mobile and desktop

---
+++ src/shared/components/Navigation/SideDrawer.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './SideDrawer.css';
+
+function SideDrawer({ children }) {
+	return <aside className="side-drawer">{children}</aside>;
+}
+
+export default SideDrawer;
